e10f1624759c8912b7810e104903d91aabdaeb10
Toss325 <targon99@yandex.ru>
2023-05-21 19:09:12 +0500

    Создали точку входа в React

--- MeditationJS/src/index.js
+++ MeditationJS/src/index.js
@@ -1,7 +1,21 @@
-function component(text) {
-    const element = document.createElement('h1');
-    element.textContent = text;
-    return element;
-  }
-  
-  document.body.prepend(component('Проект собран на Webpack'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+import './main.scss';
+
+//function component(text) {
+//    const element = document.createElement('h1');
+//    element.textContent = text;
+//    element.className = 'testclass';
+//    return element;
+//  }
+//
+//  document.body.prepend(component('Проект собран на Webpack'));
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+       <React.StrictMode>
+         <App/>
+       </React.StrictMode>
+    );
--- MeditationJS/webpack.config.js
+++ MeditationJS/webpack.config.js
@@ -1,11 +1,72 @@
+
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = (process.env.NODE_ENV === 'production');
+
+const getCssLoader = () => isProd ? MiniCssExtractPlugin.loader : 'style-loader';
+
+console.log('Loader: ', getCssLoader(), isProd);
 
 module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash:8].js',
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test:/\.pug$/,
+                loader: 'pug-loader',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    isProd ? MiniCssExtractPlugin.loader : 'style-loader',                   
+                    'css-loader', 
+                    'postcss-loader', 
+                    'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
+            },
+        ],
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: path.join(__dirname, 'src', 'template.html'),
             filename: 'index.html',
         }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            }),
     ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
 };
